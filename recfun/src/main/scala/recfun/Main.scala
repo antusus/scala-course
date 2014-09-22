77e194f4427a222b562c54273e7026d2964b77cf
Kamil Berdychowski <k.berdychowski@gmail.com>
2014-09-22 21:52:16 +0200

    Balancing brackets done

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -19,10 +19,37 @@ object Main {
         if (c < 0 || r < 0) 0 else calcPascal(c, r)
     }
 
+
     /**
      * Exercise 2
      */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+        def checkBalance(charsToCheck: List[Char], stack: List[Char], isBalanced: Boolean): Boolean = {
+            if (charsToCheck.isEmpty) isBalanced
+            else processBrackets(charsToCheck, stack, isBalanced)
+        }
+
+        def processBrackets(charsToCheck: List[Char], stack: List[Char], isBalanced: Boolean): Boolean = {
+            if (charsToCheck.head == '(' || charsToCheck.head == ')') processBracket(charsToCheck, stack)
+            else checkBalance(charsToCheck.tail, stack, isBalanced)
+        }
+
+        def processBracket(charsToCheck: List[Char], stack: List[Char]): Boolean = {
+            if (charsToCheck.head == '(') processOpeningBracket(charsToCheck, stack)
+            else processClosingBracket(charsToCheck, stack)
+        }
+
+        def processOpeningBracket(charsToCheck: List[Char], stack: List[Char]): Boolean = {
+            checkBalance(charsToCheck.tail, '(' :: stack, false)
+        }
+
+        def processClosingBracket(charsToCheck: List[Char], stack: List[Char]): Boolean = {
+            if (stack.isEmpty) false
+            else checkBalance(charsToCheck.tail, stack.tail, true)
+        }
+
+        checkBalance(chars, List[Char](), true)
+    }
 
     /**
      * Exercise 3
