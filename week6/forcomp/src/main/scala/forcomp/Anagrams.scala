219952a2b5ee23d9e5eddd608d96d3af34b164cb
Kamil Berdychowski <k.berdychowski@gmail.com>
2014-10-27 20:34:36 +0100

    subtract implemented

--- week6/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ week6/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -122,7 +122,14 @@ object Anagrams {
       * Note: the resulting value is an occurrence - meaning it is sorted
       * and has no zero-entries.
       */
-    def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+    def subtract(x: Occurrences, y: Occurrences): Occurrences = {
+        val mapx = x.toMap
+        y.foldLeft(mapx)((acc, entry) => {
+            val occurance: Int = mapx.apply(entry._1)
+            if(occurance == entry._2) acc - entry._1
+            else acc.updated(entry._1, occurance - entry._2)
+        }).toList.sortBy(touple => touple._1)
+    }
 
     /** Returns a list of all anagram sentences of the given sentence.
       *
--- week6/forcomp/src/main/scala/forcomp/Experiment.sc
+++ week6/forcomp/src/main/scala/forcomp/Experiment.sc
@@ -1,5 +1,11 @@
 object Experiment {
-    val letters = List("a", "b", "c")
-    val numbers = List(1, 2)
-    letters.zipAll(numbers, "x", 0)
+    val x = List(('a', 1), ('d', 1), ('l', 1), ('r', 2))
+    val y = List(('r', 1))
+
+    val mapx = x.toMap
+    y.foldLeft(mapx)((acc, entry) => {
+        val occurance: Int = mapx.apply(entry._1)
+        if(occurance == entry._2) acc - entry._1
+        else acc.updated(entry._1, occurance - entry._2)
+    }).toList.sortBy(touple => touple._1)
 }
