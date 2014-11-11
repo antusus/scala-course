1a06f6f440ce6ada43cf14e1ec6d4669b9a797c1
Kamil Berdychowski <k.berdychowski@gmail.com>
2014-11-11 21:33:39 +0100

    neighbours and legalNeighbours implemented

--- week7/streams/src/main/scala/streams/GameDef.scala
+++ week7/streams/src/main/scala/streams/GameDef.scala
@@ -140,13 +140,18 @@ trait GameDef {
      * Returns the list of blocks that can be obtained by moving
      * the current block, together with the corresponding move.
      */
-    def neighbors: List[(Block, Move)] = ???
+    def neighbors: List[(Block, Move)] = List(
+      (this.left, Left),
+      (this.right, Right),
+      (this.up, Up),
+      (this.down, Down)
+    )
 
     /**
      * Returns the list of positions reachable from the current block
      * which are inside the terrain.
      */
-    def legalNeighbors: List[(Block, Move)] = ???
+    def legalNeighbors: List[(Block, Move)] = neighbors.filter(elem => elem._1.isLegal)
 
     /**
      * Returns `true` if the block is standing.
--- week7/streams/src/test/scala/streams/BloxorzSuite.scala
+++ week7/streams/src/test/scala/streams/BloxorzSuite.scala
@@ -1,106 +1,127 @@
 package streams
 
-import org.scalatest.FunSuite
-
 import org.junit.runner.RunWith
+import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
-
-import Bloxorz._
 
 @RunWith(classOf[JUnitRunner])
 class BloxorzSuite extends FunSuite {
 
-  trait SolutionChecker extends GameDef with Solver with StringParserTerrain {
-    /**
-     * This method applies a list of moves `ls` to the block at position
-     * `startPos`. This can be used to verify if a certain list of moves
-     * is a valid solution, i.e. leads to the goal.
-     */
-    def solve(ls: List[Move]): Block =
-      ls.foldLeft(startBlock) { case (block, move) => move match {
-        case Left => block.left
-        case Right => block.right
-        case Up => block.up
-        case Down => block.down
-      }
+    trait SolutionChecker extends GameDef with Solver with StringParserTerrain {
+        /**
+         * This method applies a list of moves `ls` to the block at position
+         * `startPos`. This can be used to verify if a certain list of moves
+         * is a valid solution, i.e. leads to the goal.
+         */
+        def solve(ls: List[Move]): Block =
+            ls.foldLeft(startBlock) { case (block, move) => move match {
+                case Left => block.left
+                case Right => block.right
+                case Up => block.up
+                case Down => block.down
+            }
+            }
     }
-  }
 
-  trait Level1 extends SolutionChecker {
-      /* terrain for level 1*/
+    trait Level1 extends SolutionChecker {
+        /* terrain for level 1*/
 
-    val level =
-    """ooo-------
-      |oSoooo----
-      |ooooooooo-
-      |-ooooooooo
-      |-----ooToo
-      |------ooo-""".stripMargin
+        val level =
+            """ooo-------
+              |oSoooo----
+              |ooooooooo-
+              |-ooooooooo
+              |-----ooToo
+              |------ooo-""".stripMargin
 
-    val optsolution = List(Right, Right, Down, Right, Right, Right, Down)
-  }
-
-  test("terrain function level 1") {
-    new Level1 {
-      assert(terrain(Pos(0,0)), "0,0")
-      assert(!terrain(Pos(4,11)), "4,11")
+        val optsolution = List(Right, Right, Down, Right, Right, Right, Down)
     }
-  }
 
-  test("findChar level 1") {
-    new Level1 {
-      assert(startPos == Pos(1,1))
+    test("terrain function level 1") {
+        new Level1 {
+            assert(terrain(Pos(0, 0)), "0,0")
+            assert(!terrain(Pos(4, 11)), "4,11")
+        }
     }
-  }
 
-  test("block is standing") {
-    new Level1{
-      assert(Block(Pos(0,0), Pos(0,0)).isStanding)
+    test("findChar level 1") {
+        new Level1 {
+            assert(startPos == Pos(1, 1))
+        }
     }
-  }
 
-  test("block is not standing") {
-    new Level1{
-      assert(Block(Pos(0,0), Pos(0,1)).isStanding === false)
-      assert(Block(Pos(2,2), Pos(2,3)).isStanding === false)
+    test("block is standing") {
+        new Level1 {
+            assert(Block(Pos(0, 0), Pos(0, 0)).isStanding)
+        }
     }
-  }
 
-  test("block inside of terrain is legal") {
-    new Level1{
-      assert(Block(Pos(0,0), Pos(0,0)).isLegal, "0,0;0,0 is legal")
-      assert(Block(Pos(2,5), Pos(2,6)).isLegal, "2,5;2,6 is legal")
+    test("block is not standing") {
+        new Level1 {
+            assert(Block(Pos(0, 0), Pos(0, 1)).isStanding === false)
+            assert(Block(Pos(2, 2), Pos(2, 3)).isStanding === false)
+        }
     }
-  }
 
-  test("block outside of terrain is legal") {
-    new Level1{
-      assert(Block(Pos(0,3), Pos(0,4)).isLegal === false, "0,3;0,4 is not legal")
-      assert(Block(Pos(0,2), Pos(0,3)).isLegal === false, "0,2;0,3 is not legal")
-      assert(Block(Pos(0,3), Pos(0,3)).isLegal === false, "0,3;0,3 is not legal")
-      assert(Block(Pos(2,0), Pos(3,0)).isLegal === false, "2,0;3,0 is not legal")
+    test("block inside of terrain is legal") {
+        new Level1 {
+            assert(Block(Pos(0, 0), Pos(0, 0)).isLegal, "0,0;0,0 is legal")
+            assert(Block(Pos(2, 5), Pos(2, 6)).isLegal, "2,5;2,6 is legal")
+        }
     }
-  }
 
-  test("create block at starting position") {
-    new Level1 {
-      val blockAtStart = startBlock
-      assert(blockAtStart.b1.x === 1, "b1.x === 1")
-      assert(blockAtStart.b1.y === 1, "b1.y === 1")
-      assert(blockAtStart.b2.x === 1, "b2.x === 1")
-      assert(blockAtStart.b2.y === 1, "b2.y === 1")
+    test("block outside of terrain is legal") {
+        new Level1 {
+            assert(Block(Pos(0, 3), Pos(0, 4)).isLegal === false, "0,3;0,4 is not legal")
+            assert(Block(Pos(0, 2), Pos(0, 3)).isLegal === false, "0,2;0,3 is not legal")
+            assert(Block(Pos(0, 3), Pos(0, 3)).isLegal === false, "0,3;0,3 is not legal")
+            assert(Block(Pos(2, 0), Pos(3, 0)).isLegal === false, "2,0;3,0 is not legal")
+        }
     }
-  }
 
-  test("optimal solution for level 1") {
-    new Level1 {
-      assert(solve(solution) == Block(goal, goal))
+    test("create block at starting position") {
+        new Level1 {
+            val blockAtStart = startBlock
+            assert(blockAtStart.b1.x === 1, "b1.x === 1")
+            assert(blockAtStart.b1.y === 1, "b1.y === 1")
+            assert(blockAtStart.b2.x === 1, "b2.x === 1")
+            assert(blockAtStart.b2.y === 1, "b2.y === 1")
+        }
     }
-  }
 
-  test("optimal solution length for level 1") {
-    new Level1 {
-      assert(solution.length == optsolution.length)
+    test("find all neighbours for block") {
+        new Level1 {
+            val block = Block(Pos(2, 0), Pos(2, 0))
+            assert(block.neighbors ===
+                List(
+                    (block.left, Left),
+                    (block.right, Right),
+                    (block.up, Up),
+                    (block.down, Down)
+                ))
+        }
     }
-  }
+
+    test("find all legal neighbours for block") {
+        new Level1 {
+            val block = Block(Pos(2, 0), Pos(2, 0))
+            assert(block.legalNeighbors ===
+                List(
+                    (block.right, Right),
+                    (block.up, Up)
+                ))
+        }
+    }
+
+    test("optimal solution for level 1") {
+        new Level1 {
+            assert(solve(solution) == Block(goal, goal))
+        }
+    }
+
+    test("optimal solution length for level 1") {
+        new Level1 {
+            assert(solution.length == optsolution.length)
+        }
+    }
 }
